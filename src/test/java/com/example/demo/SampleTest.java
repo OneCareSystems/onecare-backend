ae3265ad1008b814f6bea68e2c7a7bd1825dd4ad
Thushajini <thushayoga1205@gmail.com>
2026-06-02 13:18:46 +0530

    backend test setup

---
+++ src/test/java/com/example/demo/SampleTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SampleTest {
+        @Test
+    void testAddition() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+    
+}
