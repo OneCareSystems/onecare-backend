5e69d55cf0f226da208fa573223d55e4cb0e37e3
Thushajini <146803353+Thushajini@users.noreply.github.com>
2026-06-13 23:03:46 +0530

    (DDP_#011)-implement smoke and integration testing setup and cl (#15)

---
+++ src/test/java/com/onecare/backend/SmokeTest.java
@@ -0,0 +1,13 @@
+package com.onecare.backend;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SmokeTest {
+
+    @Test
+    void shouldPassSmokeTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
