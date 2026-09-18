a10ed8230ede5ae69900420e4ba90b8e4600ffbc
vipusrihar <vipusrihar@gmail.com>
2026-09-18 22:56:46 +0530

    feat(exception): add AccountLockedException and handler in GlobalExceptionHandler

---
+++ src/main/java/com/onecare/backend/exception/AccountLockedException.java
@@ -0,0 +1,8 @@
+package com.onecare.backend.exception;
+
+public class AccountLockedException extends RuntimeException {
+
+    public AccountLockedException(String message) {
+        super(message);
+    }
+}
